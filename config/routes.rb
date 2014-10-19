f68225065b03c86361f4f208ab8cf9ebb9b72710
Camille Dolle <dollecamille@gmail.com>
2014-10-19 17:42:20 +0200

    added create.html.erb + changed HTML form to form_for helper + handled errors when saving a product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ControllerExercise::Application.routes.draw do
   get '/products' => 'products#index'
   get '/products/new' => 'products#new'
-  post '/products' => 'products#index'
+  post '/products' => 'products#create'
   resources :users
 
   # The priority is based upon order of creation:
